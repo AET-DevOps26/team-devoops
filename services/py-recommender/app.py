30a239de5f1431a8156dba5e49c5c43a8c341b37
f-s-h <fabian.heinrich@tum.de>
2026-05-12 11:04:55 +0200

    Implemented basic langchain service that creates a hello message using gpt-4.1-mini

--- services/py-recommender/app.py
+++ services/py-recommender/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from service import hello
 
 app = Flask("genai-service")
 
 @app.route("/hello")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    hello_message = hello()
+    return f"<p>{hello_message}</p>"
---
+++ services/py-recommender/service.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+
+from langchain.agents import create_agent
+from langchain.messages import SystemMessage, HumanMessage
+
+agent = create_agent("gpt-4.1-mini")
+
+def hello():
+    response = agent.invoke({
+        "messages": [
+            SystemMessage("You are a helpful but very sarcastic assistant. Do not write any comments, just the answer."),
+            HumanMessage("Write a welcome message.")
+        ]
+    })
+    return response["messages"][-1].content
