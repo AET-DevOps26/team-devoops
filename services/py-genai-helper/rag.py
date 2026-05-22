438febd353a5711561f6ca958347f816958c2217
f-s-h <fabian.heinrich@tum.de>
2026-05-22 16:16:16 +0200

    Fixed access to vector embeddings

--- services/py-genai-helper/rag.py
+++ services/py-genai-helper/rag.py
@@ -23,19 +23,22 @@ texts = [
 # Creates a vector store of our inputs using embeddings
 vector_store = FAISS.from_texts(texts, embedding=embeddings)
 
-# Create a retriever that returns the 3 most relevant sentences for an input
-retriever = vector_store.as_retriever(search_kwargs={"k": 3})
-
-# Create a retriever tool that searches our input
-retriever_tool = create_retriever_tool(retriever, name="kb_search", description="Search the small product / fruit knowledge base for information")
 
 def get_rag_agent():
+    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+
+    retriever_tool = create_retriever_tool(
+        retriever,
+        name="kb_search",
+        description="Search the knowledge base for information about preferences. Always use this tool before answering.",
+    )
+
     rag_agent = create_agent(
         model = "gpt-4.1-mini",
         tools = [retriever_tool],
         system_prompt=(
-            "You are a helpful assistant. "
-            "Always call the kb_search tool first to retrieve relevant context before answering any question. "
+            "You are a helpful assistant."
+            "Always call kb_search first to retrieve relevant context."
             "Base your answer strictly on what the tool returns."
         )
     )
