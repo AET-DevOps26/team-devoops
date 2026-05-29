5b8d71c63e90cb8504b9a6cb88811602ed389ab0
Raphael Frank <04.raphael.frank@gmail.com>
2026-05-29 12:16:00 +0200

    add https to infrastructure and proxy

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -3,6 +3,11 @@ output "vm_public_ip" {
   value       = azurerm_public_ip.main.ip_address
 }
 
+output "vm_fqdn" {
+  description = "Public FQDN of the VM (Azure-provided). Used as the Host in Traefik routing and Let's Encrypt."
+  value       = azurerm_public_ip.main.fqdn
+}
+
 output "ssh_connection" {
   description = "SSH connection string for the VM"
   value       = "ssh ${var.admin_username}@${azurerm_public_ip.main.ip_address}"
