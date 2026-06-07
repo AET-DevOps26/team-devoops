c2e8933606c01e1caba0ddb552227512b8a77679
FadyGergesRezk <84906847+FadyGergesRezk@users.noreply.github.com>
2026-06-08 01:56:21 +0200

    refactor: remove old flat events api and client files

--- web-client/src/features/events/api.ts
+++
@@ -1,6 +0,0 @@
-import { eventsClient } from '@/features/events/client'
-
-export async function getEventsHello(): Promise<string> {
-  const res = await eventsClient.get<string>('/hello')
-  return res.data
-}
