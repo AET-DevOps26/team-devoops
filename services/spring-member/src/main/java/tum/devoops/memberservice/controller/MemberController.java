3b1a3f722ccc8120d823e9c4a6ade5762a588442
f-s-h <fabian.heinrich@tum.de>
2026-06-09 11:53:48 +0200

    Implemented getAllMembers() in MemberController

--- services/spring-member/src/main/java/tum/devoops/memberservice/controller/MemberController.java
+++ services/spring-member/src/main/java/tum/devoops/memberservice/controller/MemberController.java
@@ -1,10 +1,25 @@
 package tum.devoops.memberservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tum.devoops.memberservice.model.MemberSummary;
+import tum.devoops.memberservice.service.MemberService;
+
+import java.util.List;
 
 @RestController
 public class MemberController {
 
-    
+    @Autowired
+    MemberService memberService;
+
+    @PreAuthorize("hasAnyRole('member', 'admin')")
+    @GetMapping("/")
+    public ResponseEntity<List<MemberSummary>> getAllMembers() {
+        return ResponseEntity.ok(memberService.getAllMembers());
+    }
+
 }
