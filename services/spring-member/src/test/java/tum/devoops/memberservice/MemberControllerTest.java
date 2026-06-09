2e2f4ca74c8c3a4dc787239e8363724f4e1b94e3
f-s-h <fabian.heinrich@tum.de>
2026-06-09 15:32:17 +0200

    Added tests for getMemberById

--- services/spring-member/src/test/java/tum/devoops/memberservice/MemberControllerTest.java
+++ services/spring-member/src/test/java/tum/devoops/memberservice/MemberControllerTest.java
@@ -12,10 +12,13 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tum.devoops.memberservice.config.SecurityConfig;
 import tum.devoops.memberservice.controller.MemberController;
+import tum.devoops.memberservice.model.Member;
 import tum.devoops.memberservice.model.MemberSummary;
 import tum.devoops.memberservice.service.MemberService;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.Mockito.when;
@@ -37,7 +40,7 @@ public class MemberControllerTest {
 
     // Test cases for createMember() endpoint
 
-    // Verifies that members are allowed to access the endpoint
+    // Verifies that a user with role "member" is allowed to get all members
     @Test
     @WithMockUser(roles = "member")
     void getMembersAllowedForMember() throws Exception {
@@ -45,7 +48,7 @@ public class MemberControllerTest {
                 .andExpect(status().isOk());
     }
 
-    // Verifies that admin are allowed to access the endpoint
+    // Verifies that a user wit role "admin" is allowed to get all members
     @Test
     @WithMockUser(roles = "admin")
     void getMembersAllowedForAdmin() throws Exception {
@@ -53,7 +56,7 @@ public class MemberControllerTest {
                 .andExpect(status().isOk());
     }
 
-    // Verifies that a user with a role other than admin and member is not allowed to access the endpoint (401 forbidden)
+    // Verifies that a user with a role other than "admin" and "member" is not allowed to get all members (401 forbidden)
     @Test
     @WithMockUser(roles = "guest")
     void getMembersForbiddenForWrongRole() throws Exception {
@@ -61,7 +64,7 @@ public class MemberControllerTest {
                 .andExpect(status().isForbidden());
     }
 
-    // Verifies that an anonymous user is not allowed to access the endpoint (403 unauthorized)
+    // Verifies that an anonymous user is not allowed to get all members (403 unauthorized)
     @Test
     @WithAnonymousUser
     void getMembersUnauthorizedForAnonymous() throws Exception {
@@ -107,4 +110,91 @@ public class MemberControllerTest {
                 .andExpect(content().json(objectMapper.writeValueAsString(list)));
     }
 
+    // Test cases for getMemberById() endpoint
+
+    // Verifies that a user with role "member" is allowed to retrieve a member by ID
+    @Test
+    @WithMockUser(roles = "member")
+    void getMemberByIdAllowedForMember() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        mockMvc.perform(get(String.format("/%s", randomId), UUID.randomUUID()))
+                .andExpect(status().isOk());
+    }
+
+    // Verifies that a user with role "admin" is allowed to retrieve a member by ID
+    @Test
+    @WithMockUser(roles = "admin")
+    void getMemberByIdAllowedForAdmin() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        mockMvc.perform(get(String.format("/%s", randomId), UUID.randomUUID()))
+                .andExpect(status().isOk());
+    }
+
+    // Verifies that a user with a role other than admin and member is not allowed to get a member by ID (403 forbidden)
+    @Test
+    @WithMockUser(roles = "guest")
+    void getMemberByIdForbiddenForWrongRole() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        mockMvc.perform(get(String.format("/%s", randomId), UUID.randomUUID()))
+                .andExpect(status().isForbidden());
+    }
+
+    // Verifies that an anonymous user is not allowed to get a member by ID (401 unauthorized)
+    @Test
+    @WithAnonymousUser
+    void getMemberByIdUnauthorizedForAnonymous() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        mockMvc.perform(get(String.format("/%s", randomId), UUID.randomUUID()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    // Verifies that the content type of the response is application/json
+    @Test
+    @WithMockUser(roles = "member")
+    void getMemberByIdContentType() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        mockMvc.perform(get(String.format("/%s", randomId), UUID.randomUUID()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    // Verifies that the entire member is returned correctly
+    @Test
+    @WithMockUser(roles = "member")
+    void getMemberByIdReturnsCorrectMember() throws Exception {
+        Member member = new Member(
+                UUID.randomUUID(),
+                "firstName",
+                "lastName",
+                "email",
+                LocalDate.now(),
+                "phoneNumber",
+                "address",
+                LocalDate.now(),
+                "information"
+        );
+
+        when(memberService.getMemberById(member.getId())).thenReturn(Optional.of(member));
+
+        mockMvc.perform(get(String.format("/%s", member.getId())))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(member)));
+    }
+
+    // Verifies that a 404 not found is returned, when no member for the given id is found
+    void getMemberByIdReturnsNotFound() throws Exception {
+        UUID randomId = UUID.randomUUID();
+        when(memberService.getMemberById(randomId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get(String.format("/%s", randomId)))
+                .andExpect(status().isNotFound());
+    }
+
+    // Verifies that an invalid ID returns a 400 bad request
+    void getMemberByIdReturnsBadRequestForInvalidId() throws Exception {
+        String invalidUuid = "invalid-uuid-12345";
+
+        mockMvc.perform(get(String.format("/%s", invalidUuid)))
+                .andExpect(status().isBadRequest());
+    }
 }
