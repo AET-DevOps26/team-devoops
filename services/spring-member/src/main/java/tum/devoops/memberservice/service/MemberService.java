8e11443a5e42cf9aa460e85b24f8e5ecd23e3fd1
f-s-h <fabian.heinrich@tum.de>
2026-06-09 15:32:01 +0200

    Placeholder for getMemberById

--- services/spring-member/src/main/java/tum/devoops/memberservice/service/MemberService.java
+++ services/spring-member/src/main/java/tum/devoops/memberservice/service/MemberService.java
@@ -1,14 +1,21 @@
 package tum.devoops.memberservice.service;
 
 import org.springframework.stereotype.Service;
+import tum.devoops.memberservice.model.Member;
 import tum.devoops.memberservice.model.MemberSummary;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class MemberService {
 
     public List<MemberSummary> getAllMembers() {
         return List.of();
+    }
+
+    public Optional<Member> getMemberById(UUID id) {
+        return null;
     }
 }
