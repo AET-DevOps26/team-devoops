4712fdf9fb20310eed052ba081ad9e18c407699c
f-s-h <fabian.heinrich@tum.de>
2026-06-30 14:50:36 +0200

    Fix

--- services/spring-letter/src/main/java/tum/devoops/letterservice/controller/LetterController.java
+++ services/spring-letter/src/main/java/tum/devoops/letterservice/controller/LetterController.java
@@ -1,13 +1,9 @@
 package tum.devoops.letterservice.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import tum.devoops.letterservice.api.LettersApi;
@@ -26,19 +22,13 @@ public class LetterController implements LettersApi {
     }
 
     @Override
-    @PostMapping(value = "/letters/mail", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> sendMail(@Valid @RequestBody MailRequest mailRequest) {
+    public ResponseEntity<Void> sendMail(MailRequest mailRequest) {
         letterService.sendMail(mailRequest);
         return ResponseEntity.noContent().build();
     }
 
     @Override
-    @PostMapping(
-        value = "/letters/pdf",
-        consumes = MediaType.APPLICATION_JSON_VALUE,
-        produces = { "application/pdf", MediaType.APPLICATION_JSON_VALUE }
-    )
-    public ResponseEntity<Resource> getPdf(@Valid @RequestBody PdfRequest pdfRequest) {
+    public ResponseEntity<Resource> getPdf(PdfRequest pdfRequest) {
         Resource pdf = letterService.getPdf(pdfRequest);
         return ResponseEntity.status(HttpStatus.OK).body(pdf);
     }
