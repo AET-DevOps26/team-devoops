70b380396976e775cdf7bad1236336c025fd3a6d
f-s-h <fabian.heinrich@tum.de>
2026-07-18 09:23:28 +0200

    Fixed async error

--- services/spring-letter/src/main/java/tum/devoops/letterservice/LetterServiceApplication.java
+++ services/spring-letter/src/main/java/tum/devoops/letterservice/LetterServiceApplication.java
@@ -2,8 +2,10 @@ package tum.devoops.letterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LetterServiceApplication {
 
     public static void main(String[] args) {
---
+++ services/spring-letter/src/main/java/tum/devoops/letterservice/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package tum.devoops.letterservice.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    @Bean(name = "taskExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("mail-dispatch-");
+        executor.initialize();
+        return executor;
+    }
+}
